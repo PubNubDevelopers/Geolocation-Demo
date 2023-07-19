71622f1a83d71041fc01b840b736cc6c3f06afd6
olivercarson-pubnub <oliver.carson@pubnub.com>
2023-07-19 11:09:56 -0700

    Removal of PubNub Demo Interaction
    Removal of PubNub Demo Interaction from the public repo.

--- js/app.js
+++ js/app.js
@@ -168,7 +168,6 @@ var redraw = function(payload) {
                 return function() {
                     infowindow.setContent(content);
                     infowindow.open(map,markdata);
-                    actionCompleted({action: 'View User Details', windowLocation: orglocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
                     google.maps.event.addListener(map,'click', function(){ 
                         infowindow.close();
                     });  
@@ -425,7 +424,6 @@ document.getElementById("newsession").addEventListener("click", function() {
             // For future feature: playback of user history
             publishMessage(GEOchannel+"."+UUID, message)
         }
-        actionCompleted({action: 'Create New Session', windowLocation: orglocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
     };
 }); 
 
@@ -435,7 +433,6 @@ document.getElementById("newinvite").addEventListener("click", function() {
         if (validateEmail(input)) {
             var body = 'Join me and share locations on the GEO Track App. Join with this link: '+window.location.href.split("?")[0]+'?channel='+GEOchannel;
             window.open('mailto:'+input+'?subject=GEO Track Invite&body='+encodeURIComponent(body));
-            actionCompleted({action: 'Send Invite', windowLocation: orglocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
         }
     }
 });
@@ -443,7 +440,6 @@ document.getElementById("newinvite").addEventListener("click", function() {
 document.getElementById("newinvitelink").addEventListener("click", function() {
     if (confirm("Do you want to copy the link to share your session?")) {     
         copyTextToClipboard(window.location.href.split("?")[0]+'?channel='+GEOchannel);
-        actionCompleted({action: 'Send Invite', windowLocation: orglocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
     }
 });
 
@@ -474,9 +470,6 @@ document.getElementById("button-changeusername").addEventListener("click", funct
 
     // For future feature: playback of user history
     publishMessage(GEOchannel+"."+UUID, message);
-    actionCompleted({action: 'Change Username', windowLocation: orglocation}); //This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
-    alert("Your username has been updated.");
-    //window.location = window.location.href.split("?")[0]+'?channel='+GEOchannel;
 }); 
 
 document.getElementById("send-greet").addEventListener("click", function() {
@@ -507,8 +500,6 @@ document.getElementById("send-greet").addEventListener("click", function() {
 
     // For future feature: playback of user history
     publishMessage(GEOchannel+"."+UUID, message);
-    actionCompleted({action: 'Send Greeting', windowLocation: orglocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
-    alert("You've updated your greeting.");
 });
 
 //Set the channel members given the channel and UUIDs
@@ -534,100 +525,3 @@ async function publishMessage(channel, message) {
         console.log(status);
     }
 }
-
-//The below function is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
-var actionCompleted = function(args) {
-    const pub = 'pub-c-c8d024f7-d239-47c3-9a7b-002f346c1849';
-    const sub = 'sub-c-95fe09e0-64bb-4087-ab39-7b14659aab47';
-    let identifier = "";
-    let action = args.action;
-    let blockDuplicateCalls = args.blockDuplicateCalls;
-    let debug = args.debug;
-    let windowLocation = args.windowLocation;
-
-    if (typeof action === 'undefined')
-    {
-        console.log('Interactive Demo Integration Error: No action provided');        
-        return;
-    }
-
-    if (typeof blockDuplicateCalls === 'undefined')
-    {
-        blockDuplicateCalls = true;
-    }
-
-    if (typeof debug === 'undefined')
-    {
-        debug = false;
-    }
-
-    //  If invoked from client-side, you can omit the window location
-    if (typeof windowLocation === 'undefined')
-        windowLocation = window.location.href;
-
-    var fetchClient = null;
-    if (typeof fetch === 'undefined')
-        fetchClient = args.fetchClient;
-    else
-        fetchClient = fetch;
-
-    let queryString = new URL(windowLocation).search.substring(1);
-    const urlParamsArray = queryString.split('&');
-    for (let i = 0; i < urlParamsArray.length; i++) {
-        if (urlParamsArray[i].startsWith('identifier') && urlParamsArray[i].includes('=')) {
-            let identifierPair = urlParamsArray[i].split('=');
-            identifier = identifierPair[1];
-        }
-    }
-    if (identifier === "") {
-        if (debug)
-        {
-            console.log('Interactive Demo Integration Error: Failed to detect identifier in URL query string');
-        }
-        return;
-    }
-    if (blockDuplicateCalls) {
-        //  This option only works if the sessionStorage object is defined (client-side only)
-        try {
-            if (!(typeof sessionStorage === 'undefined')) {
-                //  Read the id from session storage and only send the message if the message was not previous sent
-                let sessionStorageKey = "Demo_" + identifier + action;
-                let storedId = sessionStorage.getItem(sessionStorageKey);
-                if (storedId == null) {
-                    if (debug)
-                        console.log('Setting session key to avoid duplicate future messages being sent. Action: ' + action + '. Identifier: ' + identifier);
-                        sessionStorage.setItem(sessionStorageKey, "set");
-                }
-                else {
-                    //  This is a duplicate message, do not send it
-                    if (debug)
-                        console.log('Message blocked as it is a duplicate. Action: ' + action + '. Identifier: ' + identifier);
-                    return;
-                }
-            }                   
-        }
-        catch (err) {} //  Session storage is not available
-    }
-
-    if (debug)
-    {
-        console.log('Sending message. Action: ' + action + '. Identifier: ' + identifier);
-    }
-    
-    const url = `https://ps.pndsn.com/publish/${pub}/${sub}/0/demo/myCallback/${encodeURIComponent(JSON.stringify({ id: identifier, feature: action }))}?store=0&uuid=${identifier}`;
-    fetchClient(url)
-        .then(response => {
-        if (!response.ok) {
-            throw new Error(response.status + ' ' + response.statusText);
-        }
-        return response;
-    })
-        .then(data => {
-        //  Successfully set demo action with demo server
-        console.log("Guided Demo Integration success", url, data)
-    })
-        .catch(e => {
-        console.log('Interactive Demo Integration: ', e);
-    });
-    return;
-} 
